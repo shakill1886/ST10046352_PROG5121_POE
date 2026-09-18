a6a77265b6002274b689acb665a02bc16d1f52c2
shakill1886 <102148344+shakill1886@users.noreply.github.com>
2026-09-18 12:32:34 +0200

    POE 1 Username input only
    POE 1 Method for checking if username contains a underscore.

---
+++ Mavenproject75.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject75;
+
+/**
+ *
+ * @author shake
+ */
+public class Mavenproject75 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+    /// The method below Checks if the username contains a underscore.
+    public static boolean checkUsername(String username) {
+        return username.contains("_");
+        
+    }   
+}
